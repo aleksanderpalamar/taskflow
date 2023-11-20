c35ed27844fac8cf4d34453d59423f1c70c1d9e4
aleksanderpalamar <apalamar@live.com>
2023-11-20 16:07:42 -0300

    ADD: created a Organization Page Id and navbar components

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,58 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+export const Navbar = () => {
+  return (
+    <nav
+      className="fixed z-50 top-0 w-full h-14 px-4 border-b
+    shadow-sm bg-white flex items-center"
+    >
+      {/* <MobileSidebar /> */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          variant="primary"
+          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button size="sm" variant="primary" className="rounded-sm block md:hidden">
+          <Plus className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterSelectOrganizationUrl={"/organization/:id"}
+          afterCreateOrganizationUrl={"/organization/:id"}
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: "2.0rem", // 2.0rem = 32px
+                width: "2.0rem", // 2.0rem = 32px
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
