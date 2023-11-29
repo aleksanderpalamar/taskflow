ead3d92d1a5974c6d832d954a1ac07b0b6ddaa92
aleksanderpalamar <apalamar@live.com>
2023-11-29 15:39:47 -0300

    Feat: Introduce createSafeAction utility for handling schema validation and asynchronous actions and created a hook useAction

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import { create } from "@/actions/createBoard";
-import { Button } from "@/components/ui/button";
-import { useFormState} from "react-dom";
+import { CreateBoard } from "@/actions/create-board"
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} } 
-  const [state, dispatch] = useFormState(create, initialState)
+  const { execute, fieldErrors } = useAction(CreateBoard, {
+    onSuccess: (data) => {
+    console.log(data, "SUCCESS👍");
+    },
+    onError: (error) => {
+      console.error(error);
+    }
+  });
+  
+  const handleOnSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  }
 
   return (
-    <form action={dispatch} className="flex items-center gap-x-2">
+    <form action={handleOnSubmit} className="flex items-center gap-x-2">
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors}/>                    
+        <FormInput errors={fieldErrors}/>                    
       </div>
       <FormButton />
     </form>
