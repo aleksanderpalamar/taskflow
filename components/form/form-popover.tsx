fb5fa1f1cabf7aac0e1f07ca5d2c71efad401c58
aleksanderpalamar <apalamar@live.com>
2024-01-18 12:41:30 -0300

    Add unsplash-js library and implement create board functionality

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -10,10 +10,12 @@ import { useAction } from "@/hooks/use-action";
 import { CreateBoard } from "@/actions/create-board";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
-import { ReactNode } from "react";
+import { ElementRef, ReactNode, useRef } from "react";
 import { X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
+import { useRouter } from "next/navigation";
 
 interface FormPopoverProps {
   children: ReactNode;
@@ -28,22 +30,26 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const { execute, fieldErrors } = useAction(CreateBoard, {
-    onSuccess: (data) => {
-      console.log({ data });
+    onSuccess: (data) => {;      
       toast.success("Board created");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`)
     },
-    onError: (error) => {
-      console.log({ error });
+    onError: (error) => {;      
       toast.error(error);
-    }
+    },
   });
 
   const handleSubmit = (formData: FormData) => {
     const title = formData.get("Title") as string;
+    const image = formData.get("Image") as string;
 
-    execute({ title });
-  }
+    execute({ title, image });
+  };
 
   return (
     <Popover>
@@ -57,7 +63,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-zinc-600">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-zinc-600"
             variant="ghost"
@@ -67,15 +73,20 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={handleSubmit} className="space-y-4">
           <div className="space-y-4">
-            <FormInput 
+            <FormPicker id="Image" errors={fieldErrors} />
+            <FormInput
               id="Title"
               label="Board Title"
               type="text"
               errors={fieldErrors}
-            />            
+            />
           </div>
-          <FormSubmit className="w-full bg-violet-500/10 
-          text-violet-500 hover:bg-violet-500/20 transition">Create</FormSubmit>
+          <FormSubmit
+            className="w-full bg-violet-500/10 
+          text-violet-500 hover:bg-violet-500/20 transition"
+          >
+            Create
+          </FormSubmit>
         </form>
       </PopoverContent>
     </Popover>
