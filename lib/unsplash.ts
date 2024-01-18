28e0da5638cbdaaff318314e119c9dad9ffaaf21
aleksanderpalamar <apalamar@live.com>
2024-01-18 12:47:37 -0300

    Add runtime variable to unsplash.ts

--- lib/unsplash.ts
+++ lib/unsplash.ts
@@ -1,5 +1,7 @@
 import { createApi } from 'unsplash-js';
 
+export const runtime = "edge";
+
 export const unsplash = createApi({
   accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
   fetch: fetch,
