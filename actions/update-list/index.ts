6c51d9a00f68c6779c70e6b7c88d9d22bfddbd9c
aleksanderpalamar <apalamar@live.com>
2024-01-25 15:50:32 -0300

    Add update-list functionality

---
+++ actions/update-list/index.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { InputType, ReturnType } from "./types";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { UpdateList } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, id, boardId } = data;
+  let list;
+
+  try {
+    list = await db.list.update({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        }
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update board",
+    }
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return {
+    data: list,
+  };
+};
+
+export const updateList = createSafeAction(UpdateList, handler)
