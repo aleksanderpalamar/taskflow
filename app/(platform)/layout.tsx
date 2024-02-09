89e5f94a4dd0b25de2222726621257d9576fa27d
aleksanderpalamar <apalamar@live.com>
2024-02-09 11:05:48 -0300

    Adiciona arquivos e dependências para suporte a modais e consultas

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,13 +1,18 @@
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner"
+import { Toaster } from "sonner";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
-}
+};
 
-export default PlatformLayout
+export default PlatformLayout;
